9c7edfe76c772fe7a36f463eff047217af6844cb
William Elí O.L <ramun346416@gmail.com>
2022-11-29 18:14:22 -0600

    Programa ejemplo #1

---
+++ struct1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+struct fecha {
+int dia;
+int mes;
+int anio;
+int dia_del_anio;
+char nombre_mes[9];
+};
+//Un ejemplo de estructura que incluye otras
+//int main()
+void main()
+{
+    printf("Hello World");
+    
+
+    //return 0;
+}
